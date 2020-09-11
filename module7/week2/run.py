1feb32365b7499f4816aea0a5f82229bfc510b07
will <will.jaw@gmail.com>
2020-09-11 12:06:40 -0700

    Create run.py

---
+++ module7/week2/run.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+import os
+import requests
+
+path = "/data/feedback/"
+reviews = os.listdir(path)
+
+dict = {}
+
+for review in reviews:
+  with open(path + review) as f:
+    dict["title"] = f.readline().strip()
+	dict["name"] = f.readline().strip()
+	dict["date"] = f.readline().strip()
+	dict["feedback"] = f.readline().strip()
+	
+  response = requests.post("http://34.123.16.109/feedback/", json=dict)
+  response.raise_for_status()
+print(response.status_code)
+	
+	
