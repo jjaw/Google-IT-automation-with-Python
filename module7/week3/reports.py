18191b1b91f13481a6fbe282e5a6335fbeec956f
will <will.jaw@gmail.com>
2020-09-11 18:06:31 -0700

    Create reports.py
    reports.py was originally given, made modifications for optional challenges

---
+++ module7/week3/reports.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer, Table, Image
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+
+def generate(filename, title, additional_info, table_data, pie_data):
+  #added pie_data parameter for the optional challenge
+  styles = getSampleStyleSheet()
+  report = SimpleDocTemplate(filename)
+  report_title = Paragraph(title, styles["h1"])
+  report_info = Paragraph(additional_info, styles["BodyText"])
+  table_style = [('GRID', (0,0), (-1,-1), 1, colors.black),
+                ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+                ('ALIGN', (0,0), (-1,-1), 'CENTER')]
+  report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
+  empty_line = Spacer(1,20)
+  #added pie_data to build
+  report.build([report_title, empty_line, report_info, empty_line, report_table, pie_data])
+
+
+
+
+
+
