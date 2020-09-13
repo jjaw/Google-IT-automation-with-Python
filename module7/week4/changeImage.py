b9882a59d933503160956844eee28e932366b7f1
Will Jaw <willjaw@wills-mbp.lan>
2020-09-12 19:23:12 -0700

    fixed typo

--- module7/week4/changeImage.py
+++ module7/week4/changeImage.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
-fromPIL import Image
+from PIL import Image
 import os, sys
 
 path = "~/supplier-data/images/
 pictures = os.listdir(path)
 
 for pic in images:
-  outfile = "~/supplier-data/images/" + image
+  outfile = "~/supplier-data/images/" + image + ".jpeg"
   try:
     Image.open(pic).resize((600,400)).convert("RGB").save(outfile,"JPEG")
   except IOError:
