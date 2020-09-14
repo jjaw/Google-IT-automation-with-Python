22c474df550d9a1c38a8f78e774be473bd07c622
Will Jaw <willjaw@wills-mbp.lan>
2020-09-13 23:40:27 -0700

    added email functions

--- module7/week4/report_email.py
+++ module7/week4/report_email.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import os, datetime
-import json
 import reports
 
 #get the current time in GMT
@@ -22,8 +21,16 @@ def generate_pdf(path):
 if __name__ == "__main__":
   path = "~/supplier-data/descriptions/"
   title = "Process Updated on " + current_date 
+  #generate the package for pdf body
   package = generate_pdf(path)
   reports.generate_report("/tmp/processed.pdf", title, package)
+
+  #generate email information
+  sender = "automation@example.com"
+  reciever = "{}@example.com".format(os.environ["USER"])
+  subject = "Upload Completed - Online Fruit Store"
+  body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
+  attachment = "/tmp/processed.pdf"
   
 
 
