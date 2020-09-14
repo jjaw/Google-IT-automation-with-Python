b423d6aeb128e3e7caedae31f2db34b6a889f39d
Will Jaw <willjaw@wills-mbp.lan>
2020-09-13 20:18:08 -0700

    added function to generate pdf in report email

--- module7/week4/report_email.py
+++ module7/week4/report_email.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
-import os, datetime, reports
+import os, datetime
+import json
+from reports import generate
 
-path = "~/supplier-data/descriptions/"
+#get the current time in GMT
+current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+def generate_pdf(path):
+  pdf = ""
+  files = os.listdir(path)
+  for file in files:
+    if file.endswith(".txt"):
+      with open(file, 'r') as f:
+        inline = f.readlines()
+        name = inline[0].strip()
+        weight = inline[1].strip()
+        pdf += "name: " + name + "<br/>" + "weight: " + weight + "<br/><br/>"
+  return pdf
+
+if __name__ == "__main__":
+  path = "~/supplier-data/descriptions/"
+  generate_pdf(path)
+
+
+
+
+
