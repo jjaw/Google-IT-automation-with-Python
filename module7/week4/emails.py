74212e11f32af79158fa6e7a6a37617a62e95ce0
Will Jaw <willjaw@wills-mbp.lan>
2020-09-14 09:52:38 -0700

    finished email protocols

---
+++ module7/week4/emails.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import email.message
+import mimetypes
+import os.path
+import smtplib
+
+def generate_email(sender, recipient, subject, body, attachment_path = None):
+  """Generate email, default is with no attachment"""
+  # Basic Email formatting
+  message = email.message.EmailMessage()
+  message['Subject'] = subject
+  message['From'] = sender
+  message['To'] = recipient
+  message.set_content(body)
+
+  # Process the attachment and add it to the email
+  attachment_name = os.path.basename(attachment_path)
+  mime_type, _ = mimetypes.guess_type(attachment_path)
+  mime_type, mime_subtype = mime_type.split("/", 1)
+
+  with open(attachment_name, 'rb') as fp:
+    message.add_attachment(fp.read(),
+                          maintype=mime_type,
+                          subtype=mime_subtype,
+                          filename=attachment_name)
+  return message
+
+def send_email(message):
+  """Sends the message to the configured SMTP server."""
+  mail_server = smtplib.SMTP('localhost')
+  mail_server.send_message(message)
+  mail_server.quit()
+
+
