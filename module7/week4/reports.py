cd1d88fb714c3fdf2e7971c3f13e1cff483bd966
Will Jaw <willjaw@wills-mbp.lan>
2020-09-13 17:47:42 -0700

    added reports.py to generate pdf

---
+++ module7/week4/reports.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from reportlab.platypus import SimpleDocTemplate
+from reportlab.platypus import Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet
+
+def generate(filename, title, info):
+    styles = getSampleStyleSheet()
+    report = SimpleDocTemplate(filename)
+    report_title = Paragraph(title, styles["h1"])
+    report_info = Paragraph(info, styles["BodyText"])
+    empty_line = Spacer(1,20)
+
+    report.build([report_title, empty_line, report_info, empty_line])
