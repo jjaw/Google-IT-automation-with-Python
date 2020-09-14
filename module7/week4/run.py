a5da7ee2cfde806977dd1b1a31023d2c824aa823
Will Jaw <willjaw@wills-mbp.lan>
2020-09-14 15:17:16 -0700

    fixed run.py errors

--- module7/week4/run.py
+++ module7/week4/run.py
@@ -3,19 +3,19 @@ import os, sys
 import json
 import requests
 
-path = "~/supplier-data/descriptions/"
+path = "supplier-data/descriptions/"
 url = "http://localhost/fruits/"
 
 files = os.listdir(path)
 
 for file in files:
   if file.endswith("txt"):
-    with open(file, 'r') as f:
+    with open(path + file, 'r') as f:
       #grab the file name, ex. 001, 002 to use for image file
       fruit_name = os.path.splitext(file)[0]
       data = f.read()
       data = data.split("\n")
-      fruit_dic = {"name": data[0], "weight (in lbs)": str(data[1].strip(" lb")), "descriptions": data[2], "image_name": fruit_name + ".jpeg"}
+      fruit_dic = {"name": data[0], "weight": str(data[1].strip(" lb")), "description": data[2], "image_name": fruit_name + ".jpeg"}
       response = requests.post(url, json=fruit_dic)
       response.raise_for_status()
       print(response.request.url)
