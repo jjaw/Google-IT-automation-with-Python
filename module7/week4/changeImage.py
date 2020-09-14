68f2983dedaf1f6cfe570c1e8350c1dd18279d2b
Will Jaw <willjaw@wills-mbp.lan>
2020-09-14 14:47:38 -0700

    fixed errors in changeImage.py

--- module7/week4/changeImage.py
+++ module7/week4/changeImage.py
@@ -2,16 +2,16 @@
 from PIL import Image
 import os, sys
 
-path = "~/supplier-data/images/"
+path = "supplier-data/images/"
 pictures = os.listdir(path)
 
 for pic in pictures:
   if 'tiff' in pic:
     #grab the picture name without the .tiff extension
     file_name = os.path.splitext(pic)[0]
-    outfile = "~/supplier-data/images/" + file_name + ".jpeg"
+    outfile = "supplier-data/images/" + file_name + ".jpeg"
     try:
-      Image.open(pic).resize((600,400)).convert("RGB").save(outfile,"JPEG")
+      Image.open(path + pic).convert"RGB").resize((600,400)).save(outfile,"JPEG")
     except IOError:
       print("cannot convert", pic)
 
