c4f8f880f1176701d5afa9436ce5bec04d53f0c0
Will Jaw <willjaw@wills-mbp.lan>
2020-09-14 17:55:25 -0700

    fixed final bugs to complete the lab

--- module7/week4/emails.py
+++ module7/week4/emails.py
@@ -14,16 +14,15 @@ def generate_email(sender, recipient, subject, body, attachment_path = None):
   message['To'] = recipient
   message.set_content(body)
 
-  # Process the attachment and add it to the email
-  attachment_name = os.path.basename(attachment_path)
-  mime_type, _ = mimetypes.guess_type(attachment_path)
-  mime_type, mime_subtype = mime_type.split("/", 1)
-
-  with open(attachment_path, 'rb') as fp:
-    message.add_attachment(fp.read(),
-                          maintype=mime_type,
-                          subtype=mime_subtype,
-                          filename=attachment_name)
+  if attachment_path != None:
+    attachment_name = os.path.basename(attachment_path)
+    mime_type, _ = mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split("/", 1)
+    with open(attachment_path, 'rb') as fp:
+      message.add_attachment(fp.read(),
+                            maintype=mime_type,
+                            subtype=mime_subtype,
+                            filename=attachment_name)
   return message
 
 def send_email(package):
--- module7/week4/health_check.py
+++ module7/week4/health_check.py
@@ -10,10 +10,6 @@ def cpu_check():
   cpu_usage = psutil.cpu_percent(1) 
   return not cpu_usage > 80
 
-if not cpu_check():
-  subject = "Error - CPU usage is over 80%"
-  email_warning(subject)
-
 #Report an error if available disk space is lower than 20%
 def disc_space_check():  
   disk_usage = shutil.disk_usage("/")
@@ -22,33 +18,37 @@ def disc_space_check():
   threshold = disk_free / disk_total * 100
   return threshold > 20
 
-if not disc_space_check():
-  subject = "Error - Available disk space is less than 20%"
-  email_warning(subject)
-
 #Report an error if available memory is less than 500MB
 def available_memory_check():
   available = psutil.virtual_memory().available
   available_in_MB = available / 1024 ** 2 #convert to MB
   return available_in_MB > 500
 
-if not available_memory_check():
-  subject = "Error - Available memory is less than 500MB"
-  email_warning(subject)
-
 #Report an error if the hostname "localhost" cannot be resolved to "127.0.0.1"
 def hostname_check():
   local_host_ip = socket.gethostbyname('localhost')
   return local_host_ip == "127.0.0.1"
 
-if not hostname_check():
-  subject = "Error - localhost cannot be resolved to 127.0.0.1"
-  email_warning(subject)
-
 def email_warning(error):
   sender = "automation@example.com"
-  receiver = receiver = "{}@example.com".format(os.environ["USER"])
+  receiver = "{}@example.com".format(os.environ["USER"])
   subject = error
   body = "Please check your system and resolve the issue as soon as possible."
   message = emails.generate_email(sender, receiver, subject, body)
   emails.send_email(message)
+
+if not cpu_check():
+  subject = "Error - CPU usage is over 80%"
+  email_warning(subject)
+
+if not disc_space_check():
+  subject = "Error - Available disk space is less than 20%"
+  email_warning(subject)
+
+if not available_memory_check():
+  subject = "Error - Available memory is less than 500MB"
+  email_warning(subject)
+
+if not hostname_check():
+  subject = "Error - localhost cannot be resolved to 127.0.0.1"
+  email_warning(subject)
